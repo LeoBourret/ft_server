c2ce7b5a9a6587bc7559e49a13d5430be05d2b3b
LeoBourret <bourretleo@hotmail.fr>
2021-02-24 17:25:13 +0100

    Done but not so done

--- srcs/auto-index-switch.sh
+++ srcs/auto-index-switch.sh
@@ -1,1 +1,5 @@
-sed -i 's/autoindex on/autoindex off/; t; s/autoindex off/autoindex on/' nginx-block
+sed -i 's/autoindex on/autoindex off/; t; s/autoindex off/autoindex on/' /srcs_docker/nginx-block
+cp srcs_docker/nginx-block /etc/nginx/sites-available/nginx-block
+rm /etc/nginx/sites-enabled/nginx-block
+ln -s /etc/nginx/sites-available/nginx-block  /etc/nginx/sites-enabled/nginx-block
+service nginx restart
