26590aeade8b800d1257134e50cd34e13fd52951
LeoBourret <bourretleo@hotmail.fr>
2021-02-25 11:31:19 +0100

    Done but not so done

--- srcs/docker-entrypoint.sh
+++ srcs/docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-service nginx start 
-service mysql start 
-service php7.3-fpm start 
+service nginx start
+service mysql start
+service php7.3-fpm start
 
 wget -c https://wordpress.org/latest.tar.gz -O - | tar -xz -C /var/www
 chown -R www-data: /var/www/wordpress
